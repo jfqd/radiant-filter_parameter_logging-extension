e3e9b84dcf0e5e5728723dd21128686d8fb8af4b
Stefan Husch <s.husch@qutic.com>
2010-11-02 12:50:22 +0100

    first commit with content

---
+++ filter_parameter_logging_extension.rb
@@ -0,0 +1,17 @@
+# Uncomment this if you reference any of your controllers in activate
+# require_dependency 'application_controller'
+
+class FilterParameterLoggingExtension < Radiant::Extension
+  version "1.0"
+  description "Filter user, page, snippets, layouts, assets content in the log."
+  url "http://github.com/jfqd/filter_parameter_logging"
+  
+  def activate
+    ApplicationController.class_eval do
+      filter_parameter_logging :username_or_email, :password, :password_confirmation,
+                               :name, :email, :login, :authenticity_token, :content,
+                               :published_at, :title, :caption
+    end
+  end
+  
+end
